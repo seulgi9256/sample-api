15f0b1da17879b525c9e99e3046d3a88ba394d65
남궁성환 <saturn203@naver.com>
2022-02-15 21:51:59 +0900

    CrudRepository

--- src/main/java/com/example/sampleapi/repository/board/BoardRepository.java
+++ src/main/java/com/example/sampleapi/repository/board/BoardRepository.java
@@ -6,11 +6,11 @@ import com.example.sampleapi.repository.board.entity.BoardEntity;
 
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends PagingAndSortingRepository<BoardEntity, Integer> , BoardNative {
+public interface BoardRepository extends CrudRepository<BoardEntity, Integer> , BoardNative {
 
     @Modifying
     @Query(value="UPDATE board b set b.title = :title , b.contents = :contents , b.modify_id = :modifyId , b.modify_name = :modifyName , b.modify_date = :modifyDate WHERE b.num = :num")
