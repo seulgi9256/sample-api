5ebb83538c07824fbacc8639ababaf774a419c2f
남궁성환 <saturn203@naver.com>
2022-02-17 10:10:22 +0900

    dd

--- src/main/java/com/example/sampleapi/repository/board/BoardRepository.java
+++ src/main/java/com/example/sampleapi/repository/board/BoardRepository.java
@@ -15,7 +15,5 @@ public interface BoardRepository extends CrudRepository<BoardEntity, Integer> , 
     @Modifying
     @Query(value="UPDATE board b set b.title = :title , b.contents = :contents , b.modify_id = :modifyId , b.modify_name = :modifyName , b.modify_date = :modifyDate WHERE b.num = :num")
     boolean updateBoard(String title, String contents,String modifyId, String modifyName , LocalDateTime modifyDate,  int num);
- 
-    
-    
+
 }
