19d65670f898d94c6a431cde9fce302f03aad019
남궁성환 <saturn203@naver.com>
2022-02-18 17:08:06 +0900

    dd

--- src/main/java/com/example/sampleapi/SampleApiApplication.java
+++ src/main/java/com/example/sampleapi/SampleApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleApiApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApiApplication.class, args);
