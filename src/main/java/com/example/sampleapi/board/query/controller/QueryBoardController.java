9e8eca5312ec605a8b8b68af3cb8656942b319b6
binam33 <bi.nam@osckorea.com>
2022-09-29 13:10:33 +0900

    add logging

--- src/main/java/com/example/sampleapi/board/query/controller/QueryBoardController.java
+++ src/main/java/com/example/sampleapi/board/query/controller/QueryBoardController.java
@@ -2,20 +2,18 @@ package com.example.sampleapi.board.query.controller;
 
 import java.util.List;
 
-import com.example.sampleapi.board.query.dto.BoardDto;
-import com.example.sampleapi.board.query.service.QueryBoardService;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.sampleapi.board.query.dto.BoardDto;
+import com.example.sampleapi.board.query.service.QueryBoardService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,6 +34,7 @@ public class QueryBoardController {
 	@Operation(summary = "게시판 목록조회", description = "게시판 목록을 페이지로 조회합니다.")
     @GetMapping("/list")
 	public  List<BoardDto>  boardList(@RequestParam(value="size", defaultValue = "10")  int size , @RequestParam(value="page", defaultValue = "1") int page) {
+		log.info("boardList:size-{},page-{}",size,page);
 		List<BoardDto> boardList = queryService.boardList(size, page);
 		return boardList;
 	}
